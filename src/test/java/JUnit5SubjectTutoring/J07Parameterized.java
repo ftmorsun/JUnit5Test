97af2d39f45e6ff825efc1a8321d0720580ee8b7
Fatma Orsun <fatmaorsun2@gmail.com>
2021-01-17 13:52:30 -0500

    "CvsSource and Parameterized Methods"

--- src/test/java/JUnit5SubjectTutoring/J07Parameterized.java
+++ src/test/java/JUnit5SubjectTutoring/J07Parameterized.java
@@ -1,8 +1,11 @@
 package JUnit5SubjectTutoring;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 
@@ -27,5 +30,27 @@ class J07Parameterized {
 			
 			assertTrue(str.length()>=0);
 		}
+		
+		@Test
+		void upperCaseTest() {
+			String str1="Ali Can";
+			String str2="ali can";
+			String str3="ali";
+			String str4="123";
+			
+			assertEquals("ALI CAN", str1.toUpperCase());
+			assertEquals("ALI CAN", str2.toUpperCase());
+			assertEquals("ALI", str3.toUpperCase());
+			assertEquals("123", str4.toUpperCase());
+			
+		}
+		
+		@ParameterizedTest
+		@CsvSource(value= {"ALI CAN, Ali Can","ALI CAN, ali can", "ALI, ali", "123, 123"})
+		void upperCaseTest2(String s1, String s2)
+		{
+			assertEquals(s1,s2.toUpperCase());
+		}
+		
 
 }
